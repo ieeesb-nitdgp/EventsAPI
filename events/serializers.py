846b55dff40d6aaf5e6e2088b0e73dc9671f292a
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-04-27 16:09:37 +0530

    untracked files

--- events/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Post
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ('id', 'title', 'slug', 'updated_on', 'content', 'image', 'image_1', 'image_2', 'image_3', 'created_on', 'status', 'event_date')
